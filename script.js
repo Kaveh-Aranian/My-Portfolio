b5cfcae24eaa515c881a44fb90aafdbb19e4fe20
Kaveh Aranian <araniankaveh@gmail.com>
2023-02-01 23:39:17 +0300

    made a simple Javascript function to make opacity lower

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 "use strict";
 
-const projects = document.querySelector(".projects-images");
+const header = document.querySelector(".header");
+const image = document.querySelector(".projects");
 
-console.log(projects);
-const logg = function () {
-  console.log("First Project");
+const hoverNav = function (e) {
+  if (e.target.classList.contains("header-menu-link")) {
+    const link = e.target;
+    const siblings = link
+      .closest(".header-menu")
+      .querySelectorAll(".header-menu-link");
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
 };
 
-projects.addEventListener("click", logg);
+const hoverImg = function (e) {
+  if (e.target.classList.contains("project-image")) {
+    const link = e.target;
+    const siblings = link
+      .closest(".projects")
+      .querySelectorAll(".project-image");
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+header.addEventListener("mouseover", hoverNav.bind(0.7));
+header.addEventListener("mouseout", hoverNav.bind(1));
+
+image.addEventListener("mouseover", hoverImg.bind(0.7));
+image.addEventListener("mouseout", hoverImg.bind(1));
